7ad2725c479828563c0c52d53391379f02d6d4d6
Cyril Ologho <cyril.paul@andela.com>
2017-08-13 22:20:17 +0100

    add authentication middleware

---
+++ app/middleware/Authentication.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+import config from '../config';
+class Authentication {
+  static authenticate(req, res, next) {
+    if(req.headers.authorization) {
+      const authorization = req.headers.authorization.split(' ');
+      const strategy = authorization[0];
+
+      if(strategy.toLowerCase() === 'bearer') {
+        try {
+          const token = authorization[1]
+          jwt.verify(token, config.auth_secret);
+          next();
+        }
+        catch(error) {
+          res.status(401).json({
+            status: 401,
+            message: 'Authorization failed, invalid token!',
+            data: ''
+          });
+        }
+      } else {
+        res.status(401).json({
+          status: 401,
+          message: 'Authorization failed, not using the bearer strategy!',
+          data: ''
+        });
+      }
+    } else {
+      res.status(401).json({
+        status: 401,
+        message: 'Authorization failed, no token in request!',
+        data: ''
+      });
+    }
+  }
+}
+
+export default Authentication;
