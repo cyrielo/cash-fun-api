9dba6084cdb4bd1094ab4eaa58c158578580435d
Cyril Ologho <cyril.paul@andela.com>
2017-08-13 21:00:34 +0100

    add base router

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import DB from './config/DB';
-
+import config from './config/config.json';
+import BaseRouter from './routes';
 dotenv.config();
 
 
@@ -10,9 +11,10 @@ DB.connect('mongodb://localhost:27017/cashfun')
 .then(() => {
   const app = express();
   const router = express.Router();
+  const baseRouter = new BaseRouter(router);
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
-
+  app.use(`/api/${config.version.name}/`, baseRouter.routes());
   app.listen(process.env.PORT, () => {
     console.log('Application started on port', process.env.PORT);
   });
