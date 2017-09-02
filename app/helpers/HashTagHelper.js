0c548509544de90e98fbba1e4497fac788790c91
Cyril Ologho <cyril.paul@andela.com>
2017-09-02 15:46:18 +0100

    remove the # from hastags

--- app/helpers/HashTagHelper.js
+++ app/helpers/HashTagHelper.js
@@ -2,10 +2,11 @@ class HashTagHelper {
 
   static getHashTag(post) {
     const words = post.split(' ');
-    const hashtags = words.filter((word) => {
+    const hashtags = [];
+    words.filter((word) => {
       const regex = new RegExp(/(?:^|\s)(?:#)([a-zA-Z\d]+)/ig);
       if(regex.test(word)) {
-        return word;
+        hashtags.push(word.substring(1));
       }
     });
     return hashtags;
