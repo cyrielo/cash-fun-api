99233e211957911e74349f7b16c98ffa34b9c4e7
Cyril Ologho <cyril.paul@andela.com>
2017-09-05 00:15:55 +0100

    add ability to compare old hastags from new ones

--- app/helpers/HashTagHelper.js
+++ app/helpers/HashTagHelper.js
@@ -11,6 +11,29 @@ class HashTagHelper {
     });
     return hashtags;
   }
+
+  /**
+    * @method compare
+    * @param {Array} old
+    * @param {Array} newer
+    * @desc compare two set of hashtags, and return an object
+    * @return {object} object literal with key obsolete and newer
+  */
+  static compare(old, newer) {
+    let duplicate = old.filter((word) => {
+      if(newer.indexOf(word) >= 0) {
+        return word;
+      }
+    });
+    duplicate.forEach((word) => {
+      old.splice(old.indexOf(word), 1);
+      newer.splice(newer.indexOf(word), 1);
+    });
+    return {
+      obsolete: old,
+      newer
+    };
+  }
 }
 
 export default HashTagHelper;
