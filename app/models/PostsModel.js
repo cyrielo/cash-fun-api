ef5730631d99dea9035d9020f77f2cc101f968c4
Cyril Ologho <cyril.paul@andela.com>
2017-09-05 00:20:13 +0100

    refactor PostsModel

--- app/models/PostsModel.js
+++ app/models/PostsModel.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 import cloudinary from 'cloudinary';
 import fs from 'fs';
-import jwt from 'jsonwebtoken';
-import config from '../config';
 import PostsSchema from '../schema/PostsSchema';
 import UsersModel from '../models/UsersModel';
 import HashTagHelper from '../helpers/HashTagHelper';
@@ -29,29 +27,28 @@ class PostsModel {
       const caption = req.body.caption || undefined;
       const description = req.body.description || undefined;
       const uid = res.locals.user.id;
-      let has_cash = 
-        (req.body.has_cash && req.body.has_cash === 'true') ? true : false;
+      const hashCash = (req.body.has_cash && req.body.has_cash === 'true');
 
-      const newPost = { has_cash, uid };
+      const newPost = { has_cash: hashCash, uid };
       const errors = {};
 
-      if(caption) {
-        if(caption.trim().length < 1) {
+      if (caption) {
+        if (caption.trim().length < 1) {
           errors.caption = 'Caption is empty';
         } else {
           newPost.caption = caption.trim();
         }
       }
 
-      if(description) {
-        if(description.trim().length < 1) {
+      if (description) {
+        if (description.trim().length < 1) {
           errors.description = 'description is empty';
         } else {
           newPost.description = description.trim();
-        }        
+        }
       }
 
-      if(!media) {
+      if (!media) {
         errors.media = 'Make a multipart post request to upload a media file';
       } else {
         newPost.media = [];
@@ -59,7 +56,7 @@ class PostsModel {
 
       // a counter to ensure media have been uploaded before ending the request
       let counter = 1;
-      if(Object.keys(errors).length < 1) {
+      if (Object.keys(errors).length < 1) {
         media.forEach((file, index) => {
           let currentTime = new Date().getTime();
           let newFileName = `${file.destination + uid + '.' + currentTime}.jpg`;
@@ -73,7 +70,7 @@ class PostsModel {
               format: result.format,
               bytes: result.bytes
             });
-            if(counter === media.length) {
+            if (counter === media.length) {
               const NewPost = new this.PostsModel(newPost);
               NewPost.save()
               .then((savedPost) => {
@@ -100,7 +97,6 @@ class PostsModel {
                     data: errors
                   });
                 });
-
               })
               .catch((errors) => {
                 reject({
@@ -110,7 +106,7 @@ class PostsModel {
                 });
               });
             }
-            counter++;
+            counter += 1;
           });
         });
       } else {
@@ -191,33 +187,87 @@ class PostsModel {
       const uid = res.locals.user.id;
       const caption = req.body.caption || undefined;
       const description = req.body.description || undefined;
-      let has_cash = 
-        (req.body.has_cash && req.body.has_cash === 'true') ? true : false;
+      const hashCash = (req.body.has_cash && req.body.has_cash === 'true');
 
       const update = {};
 
-      if(caption) {
-        update.caption = caption;
+      if (caption) {
+        update.caption = caption.trim();
       }
 
-      if(description) {
-        update.description = description;
+      if (description) {
+        update.description = description.trim();
       }
 
-      if(postId) {
+      if (postId) {
         this.PostsModel.findById(postId)
         .then((post) => {
-          if(uid === post.uid) {
-            if(!post.has_cash) {
-              update.has_cash = has_cash;
+          if (uid === post.uid) {
+            if (!post.has_cash) {
+              update.has_cash = hashCash;
             }
-            this.PostsModel.findByIdAndUpdate(postId, update)
-            .then((updatedPost) => {
-              fulfill({
-                status: 200,
-                message: 'Post updated successfully!',
-                data: Object.assign(post, update) 
+
+            const oldPostContent = `${post.caption} ${post.description}`;
+
+            let newPostContent = (caption) ? caption.trim() : '';
+            newPostContent += (description) ? ` ${description.trim()}` : '';
+
+            const oldHashtags = HashTagHelper.getHashTag(oldPostContent);
+            const newHashtags = HashTagHelper.getHashTag(newPostContent);
+
+            const compare = HashTagHelper.compare(oldHashtags, newHashtags);
+
+            const newHashtagsInfos = [];
+            compare.newer.forEach((hashtag) => {
+              newHashtagsInfos.push({
+                title: hashtag,
+                post_id: post._id
               });
+            });
+            this.PostsModel.findByIdAndUpdate(postId, update)
+            .then(() => {
+              if (compare.obsolete.length) {
+                this.HashTagModel.deleteMultiple(compare.obsolete, [post._id])
+                .then(() => {
+                  if (compare.newer.length) {
+                    this.HashTagModel.saveMultiple(newHashtagsInfos)
+                    .then(() => {
+                      fulfill({
+                        status: 200,
+                        message: 'Post updated',
+                        data: Object.assign(post, update)
+                      });
+                    })
+                    .catch((error) => {
+                      reject(error);
+                    });
+                  }
+                })
+                .catch((error) => {
+                  reject(error);
+                });
+              } else if (compare.newer.length) {
+                this.HashTagModel.saveMultiple(newHashtagsInfos)
+                .then(() => {
+                  fulfill({
+                    status: 200,
+                    message: 'Post updated',
+                    data: Object.assign(post, update)
+                  })
+                  .catch((error) => {
+                    reject(error);
+                  });
+                })
+                .catch((error) => {
+                  reject(error);
+                });
+              } else {
+                fulfill({
+                  status: 200,
+                  message: 'Post updated',
+                  data: Object.assign(post, update)
+                });
+              }
             })
             .catch((error) => {
               reject({
@@ -229,7 +279,7 @@ class PostsModel {
             reject({
               status: 403,
               message: 'Permission denied!'
-            })
+            });
           }
         })
         .catch((error) => {
@@ -238,12 +288,12 @@ class PostsModel {
             message: 'Unable to find the post',
             error
           });
-        });        
+        });
       } else {
         reject({
           status: 400,
           message: 'Unable to update, post id not provided!'
-        })
+        });
       }
     });
   }
@@ -257,12 +307,12 @@ class PostsModel {
   */
   remove(req, res) {
     return new Promise((fulfill, reject) => {
-      const _id = req.query.id;
+      const id = req.query.id;
       const uid = res.locals.user.id;
-      this.PostsModel.findById(_id)
+      this.PostsModel.findById(id)
       .then((post) => {
-        if(uid === post.uid) {
-          this.PostsModel.remove({ _id })
+        if (uid === post.uid) {
+          this.PostsModel.remove({ _id: id })
           .then(() => {
             fulfill({
               status: 200,
@@ -279,7 +329,7 @@ class PostsModel {
           reject({
             status: 403,
             message: 'Permission denied!'
-          })
+          });
         }
       })
       .catch((error) => {
@@ -294,49 +344,50 @@ class PostsModel {
   /**
     * @method search
     * @param {Object} req
-    * @desc searches the Hastag collection and gets their postids and return 
+    * @param {String} hashtag
+    * @desc searches the Hastag collection and gets their postids and return
     * the posts
     * @return {Promise} a promise object
   */
   search(req, hashtag) {
     return new Promise((fulfill, reject) => {
-      const limit = parseInt(req.query.limit) || 5;
-      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit, 10) || 5;
+      const page = parseInt(req.query.page, 10) || 0;
 
       this.HashTagModel.search(hashtag)
       .then((response) => {
         const hashtags = response.data;
 
-        if(hashtags) {
-          const post_ids = hashtags.post_ids;
-          const pattern = { $or: []};
-          post_ids.forEach((postId) => {
-            pattern['$or'].push({ _id: postId } );
+        if (hashtags) {
+          const postIds = hashtags.post_ids;
+          const pattern = { $or: [] };
+          postIds.forEach((postId) => {
+            pattern.$or.push({ _id: postId });
           });
           this.PostsModel.find(pattern)
           .skip(page * limit)
           .limit(limit)
           .then((result) => {
-            if(result.length) {
+            if (result.length) {
               fulfill({
                 status: 200,
                 data: result
-              });              
+              });
             } else {
               reject({
                 status: 404,
                 message: 'No posts found!'
-              })
+              });
             }
           })
           .catch((error) => {
             reject(error);
-          });          
+          });
         } else {
           reject({
             status: 404,
             message: 'No posts found!'
-          })
+          });
         }
       })
       .catch((error) => {
